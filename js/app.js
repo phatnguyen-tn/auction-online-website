59e2cd4123944481b9ba41f07172861cc4b8b5c4
Phat Nguyen <tanphat1711999@gmail.com>
2019-11-18 17:40:46 +0700

    update product detail page

--- js/app.js
+++ js/app.js
@@ -1,28 +1,26 @@
 $(document).ready(function () {
-  $('.toggle-password').on('click', function () {
-    $(this).toggleClass("zmdi-eye zmdi-eye-off");
-    var input = $($(this).attr("toggle"));
-    if (input.attr("type") == "password") {
-      input.attr("type", "text");
-    } else {
-      input.attr("type", "password");
-    }
-  });
+	$('.toggle-password').on('click', function () {
+		$(this).toggleClass("zmdi-eye zmdi-eye-off");
+		var input = $($(this).attr("toggle"));
+		if (input.attr("type") == "password") {
+			input.attr("type", "text");
+		} else {
+			input.attr("type", "password");
+		}
+	});
 
-  /*------------------
-		Single Product
-	--------------------*/
-	$('.product-thumbs-track > .pt').on('click', function(){
+	/*------------------
+		  Single Product
+	  --------------------*/
+	$('.product-thumbs-track > .pt').on('click', function () {
 		$('.product-thumbs-track .pt').removeClass('active');
 		$(this).addClass('active');
 		var imgurl = $(this).data('imgbigurl');
 		var bigImg = $('.product-big-img').attr('src');
-		if(imgurl != bigImg) {
-			$('.product-big-img').attr({src: imgurl});
-			$('.zoomImg').attr({src: imgurl});
+		if (imgurl != bigImg) {
+			$('.product-big-img').attr({ src: imgurl });
+			$('.zoomImg').attr({ src: imgurl });
 		}
 	});
 
-
-	$('.product-pic-zoom').zoom();
 });
