a4e3612ebff63edbdd7691114144a47f00a427e7
Phat Nguyen <tanphat1711999@gmail.com>
2019-11-18 17:33:39 +0700

    add product detail page

--- js/app.js
+++ js/app.js
@@ -8,4 +8,21 @@ $(document).ready(function () {
       input.attr("type", "password");
     }
   });
+
+  /*------------------
+		Single Product
+	--------------------*/
+	$('.product-thumbs-track > .pt').on('click', function(){
+		$('.product-thumbs-track .pt').removeClass('active');
+		$(this).addClass('active');
+		var imgurl = $(this).data('imgbigurl');
+		var bigImg = $('.product-big-img').attr('src');
+		if(imgurl != bigImg) {
+			$('.product-big-img').attr({src: imgurl});
+			$('.zoomImg').attr({src: imgurl});
+		}
+	});
+
+
+	$('.product-pic-zoom').zoom();
 });
